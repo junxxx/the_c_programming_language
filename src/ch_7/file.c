2eee685c61ba96043b9c9423c653f0bdabd49793
junxxx <hprjunxxx@gmail.com>
2018-10-20 22:15:09 +0800

    version 2

--- src/ch_7/file.c
+++ src/ch_7/file.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* cat: concatenate files, version 1 */
+/* cat: concatenate files, version 2 */
 int main(int argc, char *argv[])
 {
     FILE *fp;
-    char *name;
     void filecopy(FILE *, FILE *);
+    char *prog = argv[0]; /* program name for errors */
+
     if (argc == 1)  /* no args; copy standard input */
         filecopy(stdin, stdout);
     else 
         while (--argc)
         {
             if ((fp = fopen(*++argv, "r")) == NULL) {
-                printf("cat: can't open %s\n", *argv);
-                return 1;
+                fprintf(stderr, "%s: can't open %s\n", prog, *argv);
+                exit(1);
             } else {
                 filecopy(fp, stdout);
                 fclose(fp);
             }
         }
+    if (ferror(stdout)) {
+        fprintf(stderr, "%s: error writing stdout\n", prog);
+        exit(2);
+    }
+
     return 0;
 }
 
