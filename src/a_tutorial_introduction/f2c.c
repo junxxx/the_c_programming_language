c3fafa772d8280f23633dc1e6a9386e68dd8c973
junxxx <hprjunxxx@gmail.com>
2019-04-05 20:32:22 +0800

    fahrenheit2celsius floating vertion;celsius 2 fahrenheit

--- src/a_tutorial_introduction/f2c.c
+++ src/a_tutorial_introduction/f2c.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /* print Fahrenheit-Celsius table
-   for fahr = 0, 20, ..., 300 */
+   for fahr = 0, 20, ..., 300; floating-point version */
 int main(int argc, const char** argv)
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;      /* lower limit of temperature table */
@@ -12,9 +12,10 @@ int main(int argc, const char** argv)
     step = 20;      /* step size */
 
     fahr = lower;
+    printf("=====Fahrenheit-Celsius Table=====\n");
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0); 
+        printf("\t%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
